e21e1dbee328a58b6305d654e9624f83c661eec1
ranilton <ranilton.c@escolar.ifrn.edu.br>
2026-01-26 00:06:09 -0300

    comenta motivoRejeicao

--- src/main/java/model/Acesso.java
+++ src/main/java/model/Acesso.java
@@ -1,20 +1,33 @@
 package model;
 
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 
 public record Acesso(
-    Long usuario_id,
-    Long setor_id,
-    LocalTime entrada,
-    LocalTime saida,
-    boolean permitido
-) {
-    public Acesso{
-        if(usuario_id == null || setor_id == null || entrada == null){
+        Long usuario_id,
+        Long setor_id,
+        LocalDateTime entrada,
+        LocalDateTime saida,
+        boolean permitido) {
+    public Acesso {
+        if (usuario_id == null || setor_id == null || entrada == null) {
             throw new IllegalArgumentException("Acesso inválido");
+
         }
-        if(saida != null && saida.isBefore(entrada)){
+        if (saida != null && saida.isBefore(entrada)) {
             throw new IllegalArgumentException("Saída antes da entrada");
         }
     }
 }
+
+/*
+
+        if (permitido && motivoRejeicao != null) {
+            throw new IllegalArgumentException("Acesso permitido não deve ter motivo de rejeição");
+        }
+
+        if (!permitido && motivoRejeicao == null) {
+            throw new IllegalArgumentException("Acesso rejeitado deve ter motivo");
+        }
+
+
+*/
